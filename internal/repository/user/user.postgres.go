dea5f5a56c7631f7d52d9950fe3bac1bd1364276
Umar Bawazir <umarbawazirfaiz@gmail.com>
2024-05-01 22:00:55 +0700

    feat: add metric sementara

--- internal/repository/user/user.postgres.go
+++ internal/repository/user/user.postgres.go
@@ -6,6 +6,8 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"log"
+	"time"
 )
 
 type userPostgresRepository struct {
@@ -23,6 +25,8 @@ func (r *userPostgresRepository) GetAll(limit, offset, query string) (*[]model.U
 }
 
 func (r *userPostgresRepository) GetById(id int) (*model.User, error) {
+	start := time.Now()
+
 	query := "SELECT id, name, email, password FROM users WHERE id = $1"
 
 	row := r.db.QueryRow(query, id)
@@ -38,10 +42,15 @@ func (r *userPostgresRepository) GetById(id int) (*model.User, error) {
 		return nil, fmt.Errorf("failed to fetch user: %v", err)
 	}
 
+	duration := time.Since(start)
+	log.Printf("GetById Execution time: %s", duration)
+
 	return &user, nil
 }
 
 func (r *userPostgresRepository) GetByEmail(email string) (*model.User, error) {
+	start := time.Now()
+
 	query := "SELECT id, name, email, password, created_at FROM users WHERE email = $1"
 
 	row := r.db.QueryRow(query, email)
@@ -57,17 +66,25 @@ func (r *userPostgresRepository) GetByEmail(email string) (*model.User, error) {
 		return nil, fmt.Errorf("failed to fetch user: %v", err)
 	}
 
+	duration := time.Since(start)
+	log.Printf("GetByEmail Execution time: %s", duration)
+
 	return &user, nil
 }
 
 func (r *userPostgresRepository) Create(ctx context.Context, request *model.User) error {
+	start := time.Now()
+
 	query := `INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`
 
-	// Execute the query
 	_, err := r.db.Exec(query, request.Name, request.Email, request.Password)
 	if err != nil {
 		return errors.New(err.Error())
 	}
+
+	duration := time.Since(start)
+	log.Printf("Create Execution time: %s", duration)
+
 	return nil
 }
 
